3530f0a60cf6edc3601fe4d4d425ca39d4a64bab
Kayode Matthew <matthewkayode116@gmail.com>
2025-04-14 08:42:45 +0100

    feat: add InsurerFactory and ClaimTest

---
+++ database/factories/InsurerFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Claim;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Insurer>
+ */
+class InsurerFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->company,
+            'code' => $this->faker->unique()->bothify('INS-###'),
+            'user_id' => User::factory(),
+            'speciality' => $this->faker->randomElement(Claim::SPECIALTIES),
+        ];
+    }
+}
