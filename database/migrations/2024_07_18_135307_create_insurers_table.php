1840ccefbee350f4a39ee236cb96ec30576062fc
omoh09 <gabrielifoga@yahoo.com>
2025-10-11 03:50:44 -0700

    chore: update migrations

--- database/migrations/2024_07_18_135307_create_insurers_table.php
+++ database/migrations/2024_07_18_135307_create_insurers_table.php
@@ -11,6 +11,7 @@ return new class extends Migration
         Schema::create('insurers', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('email')->unique();
             $table->string('code')->unique();
             $table->timestamps();
         });
--- database/migrations/2024_07_18_135321_create_claims_table.php
+++ database/migrations/2024_07_18_135321_create_claims_table.php
@@ -10,12 +10,43 @@ return new class extends Migration
     {
         Schema::create('claims', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('insurer_id')->constrained();
+            $table->string('provider_name');
+            $table->date('encounter_date');
+            $table->date('submission_date')->default(DB::raw('CURRENT_DATE'));
+            $table->string('specialty');
+            $table->unsignedTinyInteger('priority_level'); // 1 to 5
+            $table->decimal('total_amount', 10, 2);
+            $table->foreignId('batch_id')->nullable()->constrained();
             $table->timestamps();
         });
+
+        Schema::create('claim_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('claim_id')->constrained()->onDelete('cascade');
+            $table->string('name');
+            $table->decimal('unit_price', 10, 2);
+            $table->integer('quantity');
+            $table->decimal('subtotal', 10, 2); // unit_price * quantity
+            $table->timestamps();
+        });
+
+        Schema::create('batches', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('insurer_id')->constrained();
+            $table->string('provider_name');
+            $table->date('batch_date'); // Insurer processes yesterday's batch
+            $table->decimal('total_value', 10, 2)->default(0);
+            $table->enum('status', ['pending', 'processed'])->default('pending');
+            $table->timestamps();
+        });
+
     }
 
     public function down(): void
     {
         Schema::dropIfExists('claims');
+        Schema::dropIfExists('claim_items');
+        Schema::dropIfExists('batches');
     }
 }; 
