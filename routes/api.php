6c3e41e931ae3c1bc8e22500d9dba4b4275de961
omoh09 <gabrielifoga@yahoo.com>
2025-10-11 03:49:37 -0700

    chore: update routes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,18 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ClaimController;
+use App\Http\Controllers\Api\InsurerController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/claims', [ClaimController::class, 'store']);
+Route::get('/insurers', [InsurerController::class, 'index']);
+
+// Route::get('/claims', function () {
+//     return response()->json([
+//         'message' => 'server',
+//     ]);
+// });
